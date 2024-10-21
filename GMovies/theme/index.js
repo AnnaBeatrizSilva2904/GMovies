be769317abbb7d975afd965eed4299ddd0f0e0fc
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2024-10-21 12:25:10 -0300

    Continuação da atividade

---
+++ GMovies/theme/index.js
@@ -0,0 +1,9 @@
+export const theme = {
+    background: '#eab208',
+    text: '#eab208',
+}
+
+export const styles = {
+    text: { color: theme.text },
+    background: { backgroundColor: theme.background }
+}
